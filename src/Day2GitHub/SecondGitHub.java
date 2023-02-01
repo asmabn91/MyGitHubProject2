722c8aa9207d8a5cbb1ae70820903f7593dd0e4d
asma <asma@portakal.net>
2023-02-01 14:28:19 -0800

    some changed done on second class

--- src/Day2GitHub/SecondGitHub.java
+++ src/Day2GitHub/SecondGitHub.java
@@ -3,5 +3,6 @@ package Day2GitHub;
 public class SecondGitHub {
     public static void main(String[] args) {
         System.out.println("i'm learning GitHub");
+        System.out.println("i'm so motivated");
     }
 }
